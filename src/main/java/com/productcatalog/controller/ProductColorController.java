08e8317431547d74add0fbce8f841a3e35a828bc
Ronal Escobar <76713072+rony9612@users.noreply.github.com>
2021-02-06 19:55:59 -0500

    CrossOrigin

--- src/main/java/com/productcatalog/controller/ProductColorController.java
+++ src/main/java/com/productcatalog/controller/ProductColorController.java
@@ -3,6 +3,7 @@ package com.productcatalog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.productcatalog.model.ProductColor;
 import com.productcatalog.service.IProductColorService;
 
+@CrossOrigin(origins = "http://localhost:4200/%22")
 @RestController
 @RequestMapping("/ProductColor")
 public class ProductColorController {
